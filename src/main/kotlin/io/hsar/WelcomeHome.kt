ba61201112c40e2403044ede88fca31d7ddb8f2e
Sam Chen <research.hyperspace@gmail.com>
2020-02-19 18:56:01 +0000

    Initial capability for text-to-speech playback.

--- src/main/kotlin/io/hsar/WelcomeHome.kt
+++ src/main/kotlin/io/hsar/WelcomeHome.kt
@@ -1,5 +1,11 @@
 package io.hsar
 
+import io.hsar.greeting.AWSAudioGreeter
+
 class WelcomeHome {
 
+}
+
+fun main(args: Array<String>) {
+    AWSAudioGreeter().sendGreeting("Hello Sam")
 }
--- src/main/kotlin/io/hsar/greeting/AWSAudioGreeter.kt
+++ src/main/kotlin/io/hsar/greeting/AWSAudioGreeter.kt
@@ -1,11 +1,16 @@
 package io.hsar.greeting
 
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.polly.AmazonPollyClientBuilder
+import com.amazonaws.services.polly.model.OutputFormat
 import com.amazonaws.services.polly.model.SynthesizeSpeechRequest
 import com.amazonaws.services.polly.model.VoiceId.Amy
 import com.amazonaws.services.polly.model.VoiceId.Brian
 import com.amazonaws.services.polly.model.VoiceId.Emma
-import java.io.InputStream
+import javazoom.jl.player.FactoryRegistry
+import javazoom.jl.player.advanced.AdvancedPlayer
+import javax.sound.sampled.AudioFormat
+
 
 class AWSAudioGreeter : Greeter {
 
@@ -13,21 +18,26 @@ class AWSAudioGreeter : Greeter {
         private val VOICE_F_AMY = Amy
         private val VOICE_F_EMMA = Emma
         private val VOICE_M_BRIAN = Brian
+
+        private val AUDIO_FORMAT = AudioFormat(8000F, 16, 1, true, false)
     }
 
-    private val awsPollyClient = AmazonPollyClientBuilder.standard().build()
+    private val awsPollyClient = AmazonPollyClientBuilder.standard().withRegion(Regions.EU_WEST_2).build()
 
     override fun sendGreeting(greeting: String) {
         SynthesizeSpeechRequest()
                 .withEngine("standard")
                 .withVoiceId(VOICE_F_AMY)
+                .withOutputFormat(OutputFormat.Mp3)
                 .withText(greeting)
                 .let { request ->
                     awsPollyClient.synthesizeSpeech(request)
                 }
                 .audioStream
-                .let { greetingStream ->
-                    TODO("Play audio")
+                .let { audioStream ->
+                    // Add buffer for mark/reset support
+                    AdvancedPlayer(audioStream, FactoryRegistry.systemRegistry().createAudioDevice())
+                            .play()
                 }
     }
 }
